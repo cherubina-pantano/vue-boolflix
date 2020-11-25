0cb730f5380f5203854266f3a0fa27222a676523
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-25 16:13:27 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log('ook js');
+/*
+*  BOOLFLIX
+*/
+const app = new Vue({
+  el: '#app',
+  data: {
+      newSearch: '',
+      results: '',
+  },
+  methods: {
+    getMovie() {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: 'dddf18dfaffab29c0f83dd0efe0302df',
+            query: this.newSearch,
+            language: 'it-IT',
+          }
+      })
+      .then( result => {
+          console.log(result.data);
+          this.results= result.data.results;
+
+        })
+       .catch(error => {
+         console.log(error);
+       });
+    }
+
+  } //Fine methods
+});
