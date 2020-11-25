1233b037e2703b5a135de0c703e2bd197c6a54e6
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-25 18:28:04 +0100

    Milestone 2:  flag

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const app = new Vue({
   },
   methods: {
     getMovie() {
+      // per ottenere dati da API
       axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: 'dddf18dfaffab29c0f83dd0efe0302df',
@@ -17,13 +18,21 @@ const app = new Vue({
           }
       })
       .then( result => {
-          console.log(result.data);
+          // console.log(result.data);
           this.results= result.data.results;
 
         })
        .catch(error => {
          console.log(error);
        });
+    },
+    // visualizzare img o testo in base alla lingua
+    viewImgLanguage(language) {
+      if(language == 'it' || language == 'en') {
+        console.log('ciao');
+          return true;
+      }
+      return false;
     }
 
   } //Fine methods
